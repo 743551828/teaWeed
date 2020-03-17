81016dffcdb7eb31c14212093fd1aea55bfc0c9c
743551828@qq.com <743551828@qq.com@0e1a2743-8d03-4e5a-a60c-7e26af42b145>
2020-03-17 07:32:15 +0000

    update auto_identity
    git-svn-id: svn://svn6.apicloud.com/A6014022730696/teaWeed@2 0e1a2743-8d03-4e5a-a60c-7e26af42b145

---
+++ script/script/ipaddress.js
@@ -0,0 +1,6 @@
+// ipAddress = "http://47.111.140.91:8081/"
+// vr_path = "http://47.111.140.91:8081/file/"
+ipAddress = "http://47.111.140.91:8081/"
+vr_path = "http://47.111.140.91:8081/file/spec/"
+
+// ipAddress = "http://192.168.1.108:7808/"
